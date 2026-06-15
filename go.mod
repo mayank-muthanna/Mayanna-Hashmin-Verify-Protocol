f1999ea6b4684ef02c2c3d87c1a01a90943a32a6
Mayank Muthanna <mayankmuthanna.m@gmail.com>
2026-06-15 19:56:43 +0530

    Initial Commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/mayank-muthanna/Mayanna-Hashmin-Verify-Protocol
+
+go 1.22.2
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("Mayanna-Hashmin-Verify-Protocol initialized.")
+}
