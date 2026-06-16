34897bb2feb430719c56e2fc6bf81f3e376e0706
Mayank Muthanna <mayankmuthanna.m@gmail.com>
2026-06-16 22:11:13 +0530

    Added core types

---
+++ pkg/hm/types.go
@@ -0,0 +1,26 @@
+package hm
+
+type Kind string
+
+const (
+	Bool   Kind = "bool"
+	Byte   Kind = "byte"
+	String Kind = "string"
+
+	Int4  Kind = "int4"
+	Int8  Kind = "int8"
+	Int16 Kind = "int16"
+	Int32 Kind = "int32"
+	Int64 Kind = "int64"
+)
+
+type Value struct {
+	Kind Kind
+	Raw  any
+}
+
+type EncodedValue struct {
+	Kind  Kind
+	Bytes []byte
+	Bits  []uint8
+}
