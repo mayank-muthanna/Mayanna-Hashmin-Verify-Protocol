6c437b2634e7af9910bac92117197aaf053d7edb
Mayank Muthanna <mayankmuthanna.m@gmail.com>
2026-06-17 21:10:42 +0530

    Mayanna-Hashmin-verify Milestone 1: type value conding

--- cmd/hashmin/main.go
+++ cmd/hashmin/main.go
@@ -2,8 +2,23 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/mayank-muthanna/Mayanna-Hashmin-Verify-Protocol/pkg/hm"
 )
 
 func main() {
-	fmt.Println("Mayanna Hasmin Protocol")
+	value := hm.Value{
+		Kind: hm.Int8,
+		Raw:  int64(18),
+	}
+
+	encoded, err := hm.EncodeValue(value)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Mayanna HashMin Verify Protocol")
+	fmt.Println("Kind: ", encoded.Kind)
+	fmt.Println("Bits: ", encoded.Bits)
 }
