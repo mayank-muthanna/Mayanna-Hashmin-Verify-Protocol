1465c015584d7899b420a5415594f6328ead90f9
Mayank Muthanna <mayankmuthanna.m@gmail.com>
2026-06-17 20:56:40 +0530

    Added tests for Int8

--- pkg/hm/encode_test.go
+++ pkg/hm/encode_test.go
@@ -1,6 +1,8 @@
 package hm
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestEncodeBool(t *testing.T) {
 	encoded, err := EncodeValue(Value{
@@ -14,5 +16,36 @@ func TestEncodeBool(t *testing.T) {
 
 	if encoded.Bits[0] != 1 {
 		t.Fatalf("expected TRUE to be encoded as Bit '1' But received otherwise")
+	}
+}
+
+func TestEncodeInt8(t *testing.T) {
+	encoded, err := EncodeValue(Value{
+		Kind: Int8,
+		Raw:  int64(18),
+	})
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expected := []uint8{0, 0, 0, 1, 0, 0, 1, 0}
+
+	for i := range expected {
+		if encoded.Bits[i] != expected[i] {
+			t.Fatalf("wrong bit at index %d", i)
+		}
+	}
+
+}
+
+func TestEncodeInt8RejectsOutOfRange(t *testing.T) {
+	_, err := EncodeValue(Value{
+		Kind: Int8,
+		Raw:  int64(128),
+	})
+
+	if err == nil {
+		t.Fatal("Expected Error, but function suceeded for int8, where given value was 128")
 	}
 }
