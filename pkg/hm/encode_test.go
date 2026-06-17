9076053353ba63ea12097e7c95208de50c9dfefc
Mayank Muthanna <mayankmuthanna.m@gmail.com>
2026-06-17 20:14:29 +0530

    Test Boolean after EncodeValue

--- pkg/hm/encode_test.go
+++ pkg/hm/encode_test.go
@@ -1,1 +1,18 @@
 package hm
+
+import "testing"
+
+func TestEncodeBool(t *testing.T) {
+	encoded, err := EncodeValue(Value{
+		Kind: Bool,
+		Raw:  true,
+	})
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if encoded.Bits[0] != 1 {
+		t.Fatalf("expected TRUE to be encoded as Bit '1' But received otherwise")
+	}
+}
