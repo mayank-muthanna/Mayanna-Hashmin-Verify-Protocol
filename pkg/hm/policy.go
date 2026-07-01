6f2d4455b3beda410327a3f5e61271acbd756865
Mayank Muthanna <mayankmuthanna.m@gmail.com>
2026-07-01 19:09:27 +0530

    Add Policy type with comparison predicates
    Introduces the Policy struct and related types for managing predicates with different comparison operators (equal, greater than, less than). Includes methods to add predicates to policies and validate fields against a schema.

---
+++ pkg/hm/policy.go
@@ -0,0 +1,104 @@
+package hm
+
+import "fmt"
+
+type PredciateComparision string
+
+const (
+	Equal       PredciateComparision = "eq"
+	GreaterThan PredciateComparision = "gt"
+	LessThan    PredciateComparision = "lt"
+)
+
+type Predicate struct {
+	Field         SchemaField
+	PredicateType PredciateComparision
+	Value         Value
+}
+
+type Policy struct {
+	Schema     Schema
+	Predicates []Predicate
+}
+
+func NewPolicy(_schema Schema) *Policy {
+
+	return &Policy{
+		Schema: _schema,
+	}
+
+}
+
+func (policy *Policy) AddEqual(varName string, raw any) error {
+
+	field, err := policy.findField(varName)
+
+	if err != nil {
+		return err
+	}
+
+	return policy.addPredicate(field, Equal, raw)
+
+}
+
+func (policy *Policy) AddGreaterThan(varName string, value int64) error {
+
+	field, err := policy.findField(varName)
+
+	if err != nil {
+		return err
+	}
+
+	return policy.addPredicate(field, GreaterThan, value)
+
+}
+
+func (policy *Policy) AddLessThan(varName string, value int64) error {
+
+	field, err := policy.findField(varName)
+
+	if err != nil {
+		return err
+	}
+
+	return policy.addPredicate(field, LessThan, value)
+
+}
+
+// finds if field exists in policy from its attached schema
+func (policy *Policy) findField(varName string) (SchemaField, error) {
+
+	field, exists := policy.Schema.fieldMap[varName]
+
+	if !exists {
+		return SchemaField{}, fmt.Errorf("%s field not found in schema", varName)
+	}
+
+	return field, nil
+
+}
+
+// adds predicates to policy
+func (policy *Policy) addPredicate(field SchemaField, predicateType PredciateComparision, raw any) error {
+
+	err := validateValue(field, raw)
+	if err != nil {
+		return err
+	}
+
+	_value := Value{
+		Kind: SchemaField.Kind,
+		Raw:  raw,
+	}
+
+	_predicateToAdd := Predicate{
+		Field:         field,
+		PredicateType: predicateType,
+		Value:         _value,
+	}
+
+	policy.Predicates = append(policy.Predicates, _predicateToAdd)
+
+	return nil
+
+}
