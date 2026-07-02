13d1d89ae9a29c04a675a079c08e1653c63adb79
Mayank Muthanna <mayankmuthanna.m@gmail.com>
2026-07-02 11:20:52 +0530

    test prove over multiple iterations

--- pkg/mpc/prove_test.go
+++ pkg/mpc/prove_test.go
@@ -28,3 +28,33 @@ func TestProveVerify(t *testing.T) {
 		t.Fatal("proof should verify")
 	}
 }
+
+func TestProveVerifyMnyTimes(t *testing.T) {
+
+	for i := 0; i < 1000; i++ {
+
+		circuit := NewCircuit()
+
+		a := circuit.NewInput()
+		b := circuit.NewInput()
+
+		out := circuit.NewAND(a, b)
+
+		circuit.AddOutput(out)
+
+		witness := NewWitness()
+
+		witness.Set(a, 1)
+		witness.Set(b, 0)
+
+		proof, err := Prove(circuit, witness)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if !Verify(circuit, proof) {
+			t.Fatal("proof failed")
+		}
+	}
+}
